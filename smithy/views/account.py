2974ec75e8663c5c636892dc7a908d006164ce0d
sera0911 <sim11241124@gmail.com>
2021-08-13 18:16:22 +0900

    commit

--- smithy/views/account.py
+++ smithy/views/account.py
@@ -13,19 +13,19 @@ from smithy.forms import AccountUpdateForm
 class AccountCreateView(CreateView):
     model = User  # 장고에서 기본 제공해주는 모델
     form_class = UserCreationForm
-    success_url = reverse_lazy("accountapp:index")  # 성공시 돌아가는 url
-    template_name = "accountapp/create.html"  # 회원가입 할 때 보여질 html
+    success_url = reverse_lazy("smithy:index")  # 성공시 돌아가는 url
+    template_name = "smithy/create.html"  # 회원가입 할 때 보여질 html
 
 
 class AccountDetailView(DetailView):
     model = User
     context_object_name = "target_user"
-    template_name = "accountapp/detail.html"  # 모델 안에 있는 정보를 시각화해줄 페이지
+    template_name = "smithy/detail.html"  # 모델 안에 있는 정보를 시각화해줄 페이지
 
 
 
 class AccountUpdateView(UpdateView):
     model = User  # 장고에서 기본 제공해주는 모델
     form_class = AccountUpdateForm
-    success_url = reverse_lazy("accountapp:index")  # 성공시 돌아가는 url
-    template_name = "accountapp/update.html"  # 회원가입 할 때 보여질 html
+    success_url = reverse_lazy("smithy:index")  # 성공시 돌아가는 url
+    template_name = "smithy/update.html"  # 회원가입 할 때 보여질 html
