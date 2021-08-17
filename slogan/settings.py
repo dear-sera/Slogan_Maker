39144527ba9b11776b48bf74c47acbdfbd1561e1
sera0911 <sim11241124@gmail.com>
2021-08-17 15:49:28 +0900

    commit

--- slogan/settings.py
+++ slogan/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','13.209.171.193']
+ALLOWED_HOSTS = ['127.0.0.1','aislogan.shop','13.209.171.193']
 
 
 # Application definition
